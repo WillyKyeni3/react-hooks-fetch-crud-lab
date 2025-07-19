45ef58e1ac7012d32d39d5bf407e41a90269cdd9
willymusyoka <willykyeni@gmail.com>
2025-07-19 15:11:58 +0300

    Added the GET POST DELETE AND PATCH functionality to the QuestionList component.

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,65 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
 
 function App() {
   const [page, setPage] = useState("List");
+  const [questions, setQuestions] = useState([]);
+
+  // Fetch questions on initial component mount
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+      .then((r) => r.json())
+      .then((questions) => setQuestions(questions));
+  }, []);
+
+  // Callback to add a new question to state
+  function handleAddQuestion(newQuestion) {
+    setQuestions((prevQuestions) => [...prevQuestions, newQuestion]);
+  }
+
+  // Callback to delete a question from the server and state
+  function handleDeleteQuestion(id) {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "DELETE",
+    }).then(() => {
+      const updatedQuestions = questions.filter((q) => q.id !== id);
+      setQuestions(updatedQuestions);
+    });
+  }
+
+  // Callback to update a question's correct answer on the server and in state
+  function handleUpdateQuestion(id, correctIndex) {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ correctIndex }),
+    })
+      .then((r) => r.json())
+      .then((updatedItem) => {
+        const updatedQuestions = questions.map((q) => {
+          if (q.id === updatedItem.id) return updatedItem;
+          return q;
+        });
+        setQuestions(updatedQuestions);
+      });
+  }
 
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? (
+        <QuestionForm onAddQuestion={handleAddQuestion} />
+      ) : (
+        <QuestionList
+          questions={questions}
+          onDeleteQuestion={handleDeleteQuestion}
+          onUpdateQuestion={handleUpdateQuestion}
+        />
+      )}
     </main>
   );
 }
